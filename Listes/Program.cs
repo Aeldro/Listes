bb9876ba40944980e24c6a773dc0e321cad12442
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-24 09:49:07 +0200

    Ajoutez des fichiers projet.

---
+++ Listes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Listes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("aaa");
+            list.Add("bbb");
+            list.Add("ccc");
+            list.RemoveAt(0);
+            list.Remove("ccc");
+            list[0] = "ddd";
+            Console.WriteLine(list[0]);
+        }
+    }
+}
